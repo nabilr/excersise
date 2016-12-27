1fad527939b0873e4a46731194618c813cfc8e89
nabilr <nabil.rahiman@gmail.com>
2016-12-27 11:12:28 +0400

    Initial commit

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+
+@app.route('/submit',methods=['POST'])
+def submit():
+
+	#print(request.form)
+	
+	var1=request.form['nm']
+	var2=request.form['nn']
+	
+	#reference
+	#https://www.tutorialspoint.com/flask/flask_url_building.htm
+	return redirect(url_for('my_results',var1=var1,var2=var2))
+
 
 #Reference:
 #https://www.tutorialspoint.com/flask/flask_variable_rules.htm
@@ -7,7 +21,7 @@ app = Flask(__name__)
 #http://127.0.0.1:5000/name/nabil
 #here var1 is 'name' and 'var2' is nabil
 @app.route('/<var1>/<var2>')
-def results(var1, var2):
+def my_results(var1, var2):
 	print(var1, var2)
 
 	#reference 
